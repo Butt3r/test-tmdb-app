1e44344f9591511b5b2ea79db8b4460ae2c158e7
Song <oos9596@gmail.com>
2020-10-25 18:38:01 +0900

    Updated video, fetch detail data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,131 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import { Video } from './Movie';
 import config from '../api/config';
+
+import './Home.css';
+import { faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const baseUrl = config.API_ROOT;
 const api_key = config.API_KEY;
 const nowPlaying = `${baseUrl}/movie/now_playing`;
+const movieUrl = `${baseUrl}/movie`;
+
 //const topRate = `${baseUrl}/movie/top_rated`;
-//const movieId = `${baseUrl}/movie/movieID`;
 
 
 class Home extends React.Component
 {
-
-  state = {
+  constructor(props){
+    super(props);
+    this.state = {
     isLoading: true,
     movies: [],
-    counter: 0
+    videos: [],
+    counter: 19
   };
+}
+
+
 
 fetchNowPlaying = async () => {
-    const {data} = await axios.get(nowPlaying, {
+
+    const{data} = await axios.get(nowPlaying, {
       params: {
           api_key: api_key,
-          language: 'en-US',
+          language: 'ko-KR'
       }
-  })
-  console.log(data.results);
-  this.setState({movies: data['results'], isLoading: false});
+  });
+  //console.log(data.results);
+  this.setState({isLoading: false});
+  
+  let getId = data['results'].map((m) => {
+    const id = m.id
+    this.fetchMovieDetail(id);
+    this.fetchVideo(id);
+  });
+  return getId
+  
 }
+
+
+
+fetchMovieDetail = async (id) => {
+  const {data} = await axios.get(`${movieUrl}/${id}`, {
+    params: {
+      api_key: api_key,
+      language: 'ko-KR'
+    }
+  });
+
+  this.state.movies.push(data);
+  this.setState({isLoading: false});
+  //console.log(data);
+  return data;
+
+}
+
+
+fetchVideo = async (id) => {
+  const {data} = await axios.get(`${movieUrl}/${id}/videos`, {
+    params: {
+      api_key: api_key,
+    }
+  });
+  this.state.videos.push(data['results'][0]);
+  this.setState({isLoading: false});
+  //console.log(this.state.videos);
+  
+  return data;
+}
+
+
+renderVideo = () => {
+ 
+  //console.log(this.state.videos);
+  const results = this.state.videos.map((m, idx) => {  
+
+    return <Video
+    key = {idx}
+    id = {m.id}
+    name = {m.name}
+    mkey = {m.key}
+    type = {m.type}
+    />
+  });
+  
+  return results[this.state.counter];
+}
+
+
+
+counterHandler = () => {
+
+  if(this.state.counter === 20)
+      {
+        this.setState({counter: 0});
+      } 
+      else if(this.state.counter === -1)
+      {
+        this.setState({counter: 19});
+      }     
+      //console.log(this.state.counter);
+      
+}
+  
 
   componentDidMount(){
     this.fetchNowPlaying();
+  } 
+
+
+  componentDidUpdate(){
+    this.counterHandler();
   }
 
-  
+
   renderMovies = () => {
     const poster = config.API_IMAGE.default;
     const results = this.state.movies.map((m, idx) => {  
@@ -44,31 +135,38 @@ fetchNowPlaying = async () => {
       id = {m.id}
       backPoster = {poster + m.backdrop_path}
       title = {m.title}
+      original = {m.original_title}
       poster =  {poster + m.poster_path}
       overview =  {m.overview}
       rate = {m.vote_average}
+      date = {m.release_date}
       />
-    })
-    if(this.state.counter == 20)
-      this.state.counter = 0;
-      return results[this.state.counter]
+   })
+   //console.log(results[this.state.counter]);
+   return results[this.state.counter];
+  
+   
   }
+
+  
 
   render(){
     const {isLoading} = this.state;
     return <section className="container">
-      {isLoading ? (
-        <div id="loader">
+
+        {isLoading ? (<div id="loader">
           <img src = "https://data.whicdn.com/images/305380579/original.gif"  alt="load"></img>
-        </div>
-      ) : (
-      <div className="movies">
-        {this.renderMovies()}
+        </div>) : ( <div id="main-movies">
+      {this.renderMovies()}
+      </div>) } 
+
+      <div id="main-video">
+        {this.renderVideo()}
       </div>
       
 
-      )}
-      <button onClick={() => this.setState({counter: this.state.counter + 1})}>next</button>
+      <FontAwesomeIcon icon={faAngleRight} size= "2x" className= "arrowRight" style= {{zIndex: "2"}} onClick={() => this.setState({counter: this.state.counter + 1})}/>
+      <FontAwesomeIcon icon={faAngleLeft} size= "2x" className= "arrowLeft" style= {{zIndex: "2"}} onClick={() => this.setState({counter: this.state.counter - 1})}/>
       </section>
 }
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,33 +1,80 @@
 import React from 'react';
-import './Movie.css';
 import PropTypes from 'prop-types';
+import config from '../api/config';
 
 
-function Movie({idx, backPoster, title, poster, overview, rate})
+import './Movie.css';
+import { faPlayCircle } from "@fortawesome/free-regular-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const baseUrl = config.API_ROOT;
+const api_key = config.API_KEY;
+const movieUrl = `${baseUrl}/movie/`;
+const youtube = 'https://www.youtube.com/watch?v=';
+const vimeo = 'https://vimeo.com/';
+
+
+function Movie({id, title, backPoster, original, poster, overview, rate, date})
 {
+
+
     return (
-    <div key={idx}>
+    <div >
+        <img className="logo" src={"https://i.pinimg.com/originals/9b/3f/9c/9b3f9c24d9fd5f297ae433eb33d93514.png"} alt="logo"  width="150" height="60"/>
     <div className="home-container">
         <img className = "fullScren" src={backPoster} alt={title} title={title}/>
+        <div className="box"></div>
+        <div className="binet-box"></div>
     </div>
     <div className="nowplaying-data" style = {{textAlign: "center", fontSize: 20}}>
-            <h2 className="title" style = {{color: "#fff"}}>{title}</h2>
-            <h3 className='rate' style = {{color: "#fff"}}>{rate/2}</h3>
+            <h1 className="title" style = {{color: "#fff"}}>{title}</h1>
+            <div className="original">{original}
+            <p className="date" style = {{color: "#fff"}}>{date.slice(0, 4)}</p></div>
+            {/* <p className="overview" style = {{color: "#fff"}}>{overview}</p> */}
+            <h3 className="rate">{rate}</h3>
         </div>
+        {/* <h1>{id}</h1> */}
+        {/* <FontAwesomeIcon icon={faPlayCircle} size= "10x" className="play" onClick = {PlayTrailer}/> */}
     </div>
     );
-    
 }
 
+export function Video({mkey})
+{
+
+    return(
+    <div>
+        <div className="video-container">
+            {/* <video id="video">
+                <source src={`${youtube}${mkey}`} type="video/mp4" />
+            </video> */}
+        {/* <vidio src={`${youtube}${mkey}`} onClick = {this.play()}/> */}
+    <i className="play" onClick={() => window.open(`${youtube}${mkey}`)}><img src="../../circled-play.png" /></i>
+    </div>
+    </div>
+    );
+}
+
+// export const playTrailer = (e) => {
+//     e.preventDefault();
+//     var vid = document.getElementById("video");
+//     vid.play();
+// }
+
+
+Video.propTypes = {
+    mkey: PropTypes.string.isRequired
+};
 
 Movie.propTypes = {
     id: PropTypes.number.isRequired,
     backPoster: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
+    original: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
     overview: PropTypes.string.isRequired,
     rate: PropTypes.number.isRequired,
-    
+    date: PropTypes.string.isRequired
 };
 
 export default Movie;
